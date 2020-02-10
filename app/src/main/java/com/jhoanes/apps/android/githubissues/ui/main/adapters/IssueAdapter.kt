89ed63f64922093944ff3ad94f49d997b8755cce
Jhoanes Freitas <jhoanesbarbosa@gmail.com>
2020-02-10 01:57:14 -0300

    Changes in issues item layout

--- app/src/main/java/com/jhoanes/apps/android/githubissues/ui/main/adapters/IssueAdapter.kt
+++ app/src/main/java/com/jhoanes/apps/android/githubissues/ui/main/adapters/IssueAdapter.kt
@@ -6,11 +6,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.jhoanes.apps.android.githubissues.R
 import com.jhoanes.apps.android.githubissues.applications.GIApplication.Companion.context
 import com.jhoanes.apps.android.githubissues.models.IssueModel
 import com.jhoanes.apps.android.githubissues.services.ViewCallback
 import com.jhoanes.apps.android.githubissues.ui.details.DetailActivity
+import de.hdodenhof.circleimageview.CircleImageView
 
 class IssueAdapter(private val callback: ViewCallback<IssueModel>) :
     RecyclerView.Adapter<IssueAdapter.IssueHolder>() {
@@ -31,6 +33,10 @@ class IssueAdapter(private val callback: ViewCallback<IssueModel>) :
         holder.mTitleTV?.text = issues[position].title
         holder.mStatusTV?.text = issues[position].state
 
+        Glide.with(context)
+            .load(issues[position].user?.avatar)
+            .into(holder.profileImage)
+
         holder.cardView?.setOnClickListener {
             callback.showProgressCentral()
             callback.startActivity(issues[position], DetailActivity::class)
@@ -43,6 +49,7 @@ class IssueAdapter(private val callback: ViewCallback<IssueModel>) :
     }
 
     class IssueHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val profileImage = itemView.findViewById<CircleImageView>(R.id.profile_image)
         val cardView = itemView.findViewById<CardView?>(R.id.cardView)
         val mTitleTV = itemView.findViewById<TextView?>(R.id.title)
         val mStatusTV = itemView.findViewById<TextView?>(R.id.status)
